8fd89539ea6643216a36bb59525a249579e983c4
AndyWatt83 <andy.watt83@gmail.com>
2017-06-25 18:07:16 -0500

    Initial folder setup and basic configuration
    [squash] Some changes to the folder structure
    
    [squash] further messing about with the folder structure
    
    Adding solution file
    
    Adding a reference to the Optimise.Logic project
    
    Adding a unit test folder
    
    Got some basic unit testing in

---
+++ src/Optimise.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Optimise.Logic;
+
+namespace Optimise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TestingVSCode test = new TestingVSCode(55);
+            Console.WriteLine("Number returned from Test class is {0}.", test.ANumber);
+              }
+    }
+}
---
+++ src/Optimise.Logic/TestingVSCode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Optimise.Logic
+{
+    public class TestingVSCode
+    {
+        int qwertyuiop;
+
+        public TestingVSCode(int passedNumber){
+            qwertyuiop = passedNumber;
+        }
+
+        public int ANumber{get{ return qwertyuiop; }}
+    }
+}
